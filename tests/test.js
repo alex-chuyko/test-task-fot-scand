e1271fee394c1cc89fea572aa4d46759d99bcf48
alex-chuyko <alex.chuyko9@gmail.com>
2017-04-19 12:04:21 +0300

    added tests

--- tests/test.js
+++ tests/test.js
@@ -28,3 +28,34 @@ describe("Circle.setRadius", function() {
 
 });
 
+describe("Circle.move", function() {
+
+    it("круг не двигается, если мышь находится не рядом с ним", function() {
+        var eventObj = document.createEvent('MouseEvents'); 
+        var oldCircleX = smallCircleObj.domElement.style.left;
+        var oldCircleY = smallCircleObj.domElement.style.top;
+        for(var i = 150; i < 160; i += 1) {
+            eventObj.initMouseEvent( 'mousemove', true, true, window, 1, i, i, parseInt(smallCircleObj.domElement.style.left) + 50, parseInt(smallCircleObj.domElement.style.top) + 50, 
+                                    false, false, true, false, 0, null );
+            bigCircleObj.domElement.dispatchEvent(eventObj);
+        }
+        assert.equal(smallCircleObj.domElement.style.left, oldCircleX);
+        assert.equal(smallCircleObj.domElement.style.top, oldCircleY);
+    });
+
+    it("круг двигается, если мышь находится рядом ним", function() {
+        var eventObj = document.createEvent('MouseEvents'); 
+        var oldCircleX = parseInt(smallCircleObj.domElement.style.left);  
+        var oldCircleY = smallCircleObj.domElement.style.top;
+        for(var i = 150; i < 160; i += 1) {
+            eventObj.initMouseEvent( 'mousemove', true, true, window, 1, i, i, parseInt(smallCircleObj.domElement.style.left) + 25, parseInt(smallCircleObj.domElement.style.top) + 25, 
+                                    false, false, true, false, 0, null );
+            bigCircleObj.domElement.dispatchEvent(eventObj);
+            oldCircleX -= 5;
+        }
+        assert.equal(smallCircleObj.domElement.style.left, oldCircleX + "px");
+        assert.equal(smallCircleObj.domElement.style.top, oldCircleY);
+    });
+
+});
+
